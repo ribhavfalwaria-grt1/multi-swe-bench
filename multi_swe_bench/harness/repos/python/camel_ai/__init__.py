5d5eab0c5ddf1ad8756b68b7dd96f66cade8a6fe
japjeet-ethara <japjeet@ethara.ai>
2026-03-08 23:16:28 +0530

    feat:camel_ai unregistered repo

--- multi_swe_bench/harness/repos/python/camel_ai/__init__.py
+++ multi_swe_bench/harness/repos/python/camel_ai/__init__.py
@@ -1,2 +1,6 @@
-from multi_swe_bench.harness.repos.python.camel_ai.camel_258_to_242 import *
 from multi_swe_bench.harness.repos.python.camel_ai.camel_223_to_210 import *
+from multi_swe_bench.harness.repos.python.camel_ai.camel_258_to_242 import *
+from multi_swe_bench.harness.repos.python.camel_ai.camel_566_to_288 import *
+from multi_swe_bench.harness.repos.python.camel_ai.camel_852_to_647 import *
+from multi_swe_bench.harness.repos.python.camel_ai.camel_1644_to_716 import *
+from multi_swe_bench.harness.repos.python.camel_ai.camel_3775_to_1726 import *
