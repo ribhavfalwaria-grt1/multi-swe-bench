ec39166ba619852076396f43dcb0d5b5dd5a09d2
Rakshit Pandey <rakshit.pandey@ethara.ai>
2026-03-22 23:48:30 +0530

    tornado registry updated

--- multi_swe_bench/harness/repos/python/tornadoweb/__init__.py
+++ multi_swe_bench/harness/repos/python/tornadoweb/__init__.py
@@ -1,2 +1,4 @@
 from multi_swe_bench.harness.repos.python.tornadoweb.tornado_3469_to_3294 import *
 from multi_swe_bench.harness.repos.python.tornadoweb.tornado_2307_to_2251 import *
+from multi_swe_bench.harness.repos.python.tornadoweb.tornado_3087_to_2018 import *
+from multi_swe_bench.harness.repos.python.tornadoweb.tornado_3585_to_3029 import *
