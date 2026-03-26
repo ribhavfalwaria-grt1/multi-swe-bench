caf89af42c5e738b30885697d517ce1f7225461e
Abhijeet <abhijeet.chaurasiya@ethara.ai>
2026-03-26 08:40:08 +0000

    Added registry for hyperium

---
+++ multi_swe_bench/harness/repos/rust/hyperium/__init__.py
@@ -0,0 +1,5 @@
+from multi_swe_bench.harness.repos.rust.hyperium.hyper_3935_to_3185 import *
+from multi_swe_bench.harness.repos.rust.hyperium.hyper_3796_to_2216 import *
+from multi_swe_bench.harness.repos.rust.hyperium.hyper_2186_to_2066 import *
+from multi_swe_bench.harness.repos.rust.hyperium.hyper_1913_to_1538 import *
+from multi_swe_bench.harness.repos.rust.hyperium.hyper_1454_to_1220 import *
